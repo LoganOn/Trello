ba7119f678758fe5f13d1cf14f7c089d57961966
Konrad Krawczyk <logan15.krawczyk@gmail.com>
2020-06-26 10:15:01 +0200

    created  mutations

--- src/main/java/resolver/Queries.java
+++ src/main/java/resolver/Queries.java
@@ -12,6 +12,7 @@ import org.eclipse.microprofile.graphql.Source;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 
 @GraphQLApi
 public class Queries {
@@ -37,6 +38,69 @@ public class Queries {
   @Query("findBoardById")
   public Board getBoard(int id){
     return trelloService.getBoard(id);
+  }
+
+  @Transactional
+  @Mutation("addBoard")
+  public Board addBoard(Board board){
+    trelloService.addBoard(board);
+    return board;
+  }
+
+  @Transactional
+  @Mutation("updateTaskBoard")
+  public Task updateTaskBoard(int taskId, int boardId){
+    trelloService.updateTaskBoard(taskId, boardId);
+    return null;
+  }
+
+  @Transactional
+  @Mutation("deleteBoardById")
+  public Board deleteBoardById(int boardId){
+    trelloService.deleteBoardById(boardId);
+    return null;
+  }
+
+  @Transactional
+  @Mutation("updateBoardPosition")
+  public Board updateBoardPosition(int boardId, int position){
+    trelloService.updateBoardPosition(boardId, position);
+    return null;
+  }
+
+  @Transactional
+  @Mutation("updateBoardTitle")
+  public Board updateBoardTitle(int boardId, String title){
+    trelloService.updateBoardTitle(boardId, title);
+    return null;
+  }
+
+  @Transactional
+  @Mutation("addTask")
+  public Task addTask(Task task){
+    trelloService.addTask(task);
+    return task;
+  }
+
+  @Transactional
+  @Mutation("deleteTaskById")
+  public Task deleteTaskById(int taskId){
+    trelloService.deleteTaskById(taskId);
+    return null;
+  }
+
+  @Transactional
+  @Mutation("updateTaskTitle")
+  public Task updateTaskTitle(int taskId, String title){
+    trelloService.updateTaskTitle(taskId, title);
+    return null;
+  }
+
+  @Transactional
+  @Mutation("updateTaskDescription")
+  public Task updateTaskDescription(int taskId, String description){
+    trelloService.updateTaskDescription(taskId, description);
+    return null;
   }
 
 }
--- src/main/java/resolver/TrelloService.java
+++ src/main/java/resolver/TrelloService.java
@@ -22,6 +22,12 @@ public class TrelloService {
   private List<Task> taskList = new ArrayList<>();
 
   @Inject
+  private BoardRepository boardRepository;
+
+  @Inject
+  private TaskRepository taskRepository;
+
+  @Inject
   public TrelloService(BoardRepository boardRepository, TaskRepository taskRepository){
     boardList = boardRepository.listAll();
     taskList = taskRepository.listAll();
@@ -45,18 +51,49 @@ public class TrelloService {
   }
 
   public void addBoard(Board board){
-    boardList.add(board);
+    boardRepository.persist(board);
   }
 
-  public void deleteBoard(int id){
-    boardList.remove(id);
+  public void deleteBoardById(int id){
+    boardRepository.delete(boardRepository.findById(Long.valueOf(id)));
   }
 
+  // dont work
   public void addTask(Task task){
-    taskList.add(task);
+    taskRepository.persist(task);
   }
 
-  public void deleteTask(int id){
-    taskList.remove(id);
+  public void deleteTaskById(int id){
+    taskRepository.delete(taskRepository.findById(Long.valueOf(id)));
+  }
+
+  public void updateTaskBoard(int taskId, int boardId){
+    Task task = taskRepository.findById(Long.valueOf(taskId));
+    task.setBoard(boardRepository.findById(Long.valueOf(boardId)));
+    taskRepository.persist(task);
+  }
+
+  public void updateBoardPosition(int boardId, int position){
+    Board board = boardRepository.findById(Long.valueOf(boardId));
+    board.setPosition(position);
+    boardRepository.persist(board);
+  }
+
+  public void updateBoardTitle(int boardId, String title){
+    Board board = boardRepository.findById(Long.valueOf(boardId));
+    board.setTitle(title);
+    boardRepository.persist(board);
+  }
+
+  public void updateTaskTitle(int taskId, String title){
+    Task task = taskRepository.findById(Long.valueOf(taskId));
+    task.setTitle(title);
+    taskRepository.persist(task);
+  }
+
+  public void updateTaskDescription(int taskId, String description){
+    Task task = taskRepository.findById(Long.valueOf(taskId));
+    task.setDescription(description);
+    taskRepository.persist(task);
   }
 }
